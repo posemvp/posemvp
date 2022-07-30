Fixed angle calculation logic

exercises_detector/yoga/warrior_II_pose.py
@@ -1,29 +1,14 @@
 from typing import List
-import numpy as np
 from calc.dtw_distance import get_dtw_distance
+from pose.landmarks import get_landmarks_vectors
 
 
 def get_warrior_II_pose_result(
-    input_pose_landmarks, image_pose_landmarks, image_joint_angles, input_joint_angles
+        input_pose_landmarks, image_pose_landmarks, image_joint_angles, input_joint_angles
 ) -> List[float]:
-    input_vector_x = []
-    input_vector_y = []
-    image_vector_x = []
-    image_vector_y = []
+    image_key_points_vector_x, image_key_points_vector_y = get_landmarks_vectors(image_pose_landmarks)
+    input_key_points_vector_x, input_key_points_vector_y = get_landmarks_vectors(input_pose_landmarks)
 
-    for key, key_point in input_pose_landmarks.items():
-        input_vector_x.append(key_point.to_dict()["normalized_point3d"]["x"])
-        input_vector_y.append(key_point.to_dict()["normalized_point3d"]["y"])
-
-    for key, key_point in image_pose_landmarks.items():
-        image_vector_x.append(key_point.to_dict()["normalized_point3d"]["x"])
-        image_vector_y.append(key_point.to_dict()["normalized_point3d"]["y"])
-
-    input_x_points = np.array(input_vector_x)
-    input_y_points = np.array(input_vector_y)
-    image_x_points = np.array(image_vector_x)
-    image_y_points = np.array(image_vector_y)
-
-    x_distance = get_dtw_distance(input_x_points, image_x_points)
-    # y_distance = get_dtw_distance(input_y_points, image_y_points)
+    x_distance = get_dtw_distance(input_key_points_vector_x, image_key_points_vector_x)
+    # y_distance = get_dtw_distance(input_key_points_vector_y, image_key_points_vector_y)
     return [x_distance]

pose/joint_angles.py
@@ -1,51 +1,67 @@
 import math
+import numpy as np
 
 
-def __get_angle(A, B, C):
+def __get_angle_using_cosine(A, B, C):
     AB = math.sqrt(math.pow(B.x - A.x, 2) + math.pow(B.y - A.y, 2))
     BC = math.sqrt(math.pow(B.x - C.x, 2) + math.pow(B.y - C.y, 2))
     CA = math.sqrt(math.pow(C.x - A.x, 2) + math.pow(C.y - A.y, 2))
     return math.acos((BC * BC + AB * AB - CA * CA) / (2 * BC * AB)) * 180 / math.pi
 
 
+def __get_angle_using_tangent(A, B, C):
+    ang = math.degrees(math.atan2(C.y - B.y, C.x - B.x) - math.atan2(A.y - B.y, A.x - B.x))
+    return ang + 360 if ang < 0 else ang
+
+
+def __get_angle_for_3d_points(A, B, C):
+    a = np.array([A.x, A.y, A.z])
+    b = np.array([B.x, B.y, B.z])
+    c = np.array([C.x, C.y, C.z])
+    ba = a - b
+    bc = c - b
+    cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
+    return np.degrees(np.arccos(cosine_angle))
+
+
 def get_joint_angles(landmarks) -> dict:
     return {
-        "RIGHT_ELBOW_ANGLE": __get_angle(
+        "RIGHT_ELBOW_ANGLE": __get_angle_using_tangent(
             landmarks["RIGHT_WRIST"].normalizedPoint3d,
             landmarks["RIGHT_ELBOW"].normalizedPoint3d,
             landmarks["RIGHT_SHOULDER"].normalizedPoint3d,
         ),
-        "RIGHT_SHOULDER_ANGLE": __get_angle(
+        "RIGHT_SHOULDER_ANGLE": __get_angle_using_tangent(
             landmarks["RIGHT_ELBOW"].normalizedPoint3d,
             landmarks["RIGHT_SHOULDER"].normalizedPoint3d,
             landmarks["RIGHT_HIP"].normalizedPoint3d,
         ),
-        "RIGHT_HIP_ANGLE": __get_angle(
+        "RIGHT_HIP_ANGLE": __get_angle_using_tangent(
             landmarks["RIGHT_SHOULDER"].normalizedPoint3d,
             landmarks["RIGHT_HIP"].normalizedPoint3d,
             landmarks["RIGHT_KNEE"].normalizedPoint3d,
         ),
-        "RIGHT_KNEE_ANGLE": __get_angle(
+        "RIGHT_KNEE_ANGLE": __get_angle_using_tangent(
             landmarks["RIGHT_HIP"].normalizedPoint3d,
             landmarks["RIGHT_KNEE"].normalizedPoint3d,
             landmarks["RIGHT_ANKLE"].normalizedPoint3d,
         ),
-        "LEFT_ELBOW_ANGLE": __get_angle(
+        "LEFT_ELBOW_ANGLE": __get_angle_using_tangent(
             landmarks["RIGHT_WRIST"].normalizedPoint3d,
             landmarks["RIGHT_ELBOW"].normalizedPoint3d,
             landmarks["RIGHT_SHOULDER"].normalizedPoint3d,
         ),
-        "LEFT_SHOULDER_ANGLE": __get_angle(
+        "LEFT_SHOULDER_ANGLE": __get_angle_using_tangent(
             landmarks["LEFT_ELBOW"].normalizedPoint3d,
             landmarks["LEFT_SHOULDER"].normalizedPoint3d,
             landmarks["LEFT_HIP"].normalizedPoint3d,
         ),
-        "LEFT_HIP_ANGLE": __get_angle(
+        "LEFT_HIP_ANGLE": __get_angle_using_tangent(
             landmarks["LEFT_SHOULDER"].normalizedPoint3d,
             landmarks["LEFT_HIP"].normalizedPoint3d,
             landmarks["LEFT_KNEE"].normalizedPoint3d,
         ),
-        "LEFT_KNEE_ANGLE": __get_angle(
+        "LEFT_KNEE_ANGLE": __get_angle_using_tangent(
             landmarks["LEFT_HIP"].normalizedPoint3d,
             landmarks["LEFT_KNEE"].normalizedPoint3d,
             landmarks["LEFT_ANKLE"].normalizedPoint3d,

samples/code/human_pose_compare1.py
@@ -68,7 +68,9 @@ def hconcat_2_videos_list(frame_list1, frame_list2):
 def get_cos_two_vector(
     vector1, vector2
 ):  # Returns the cosine similarity of two vectors
-    return np.dot(vector1, vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2))
+    return np.dot(vector1, vector2) / (
+        np.linalg.norm(vector1) * np.linalg.norm(vector2)
+    )
 
 
 def get_euclidean_distance(